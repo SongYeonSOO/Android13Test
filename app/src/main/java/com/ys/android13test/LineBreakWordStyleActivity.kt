81624dfa81ea784af6673539e29cea9d4512ca3c
SongYeonSOO <lucklys324@gmail.com>
2023-01-08 02:12:47 +0900

    LineBreakWordStyle attribute test

---
+++ app/src/main/java/com/ys/android13test/LineBreakWordStyleActivity.kt
@@ -0,0 +1,12 @@
+package com.ys.android13test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.*
+
+class LineBreakWordStyleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_line_break_word_style)
+    }
+}
